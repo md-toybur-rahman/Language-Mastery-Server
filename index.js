7ac25b5c65ef68efce1551525d080ef81bb55214
md-toybur-rahman <toyburrahman735@gmail.com>
2023-06-11 12:44:40 +0600

    cart delete oparetion

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z50rydu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -74,9 +74,15 @@ async function run() {
         app.get('/cart', async (req, res) => {
             let query = {};
             if (req.query?.email) {
-                query = { email: req.query.email }
+                query = { user_email: req.query.email }
             }
             const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.delete('/card/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId._id};
+            const result = cartCollection.deleteOne(query);
             res.send(result);
         })
 
