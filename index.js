e1113e21eca6fe79634da5c7ff5f9330a60064a8
md-toybur-rahman <toyburrahman735@gmail.com>
2023-06-11 22:30:53 +0600

    jwt and admin middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -9,6 +10,23 @@ app.use(cors());
 app.use(express.json());
 
 
+// jwt middleware
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z50rydu.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,6 +50,22 @@ async function run() {
         const instructorsCollection = client.db("language-mastery-DB").collection("instructors")
         const usersCollection = client.db("language-mastery-DB").collection("users")
         const cartCollection = client.db("language-mastery-DB").collection("cart")
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        })
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.type !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access' });
+            }
+            next();
+        }
 
         app.post('/classes', async (req, res) => {
             const singleClass = req.body;
@@ -61,7 +95,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -79,9 +113,10 @@ async function run() {
             const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
-        app.delete('/card/:id', async (req, res) => {
+
+        app.delete('/cart/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId._id};
+            const query = { _id: new ObjectId(id) };
             const result = cartCollection.deleteOne(query);
             res.send(result);
         })
